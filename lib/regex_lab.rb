f0fb5a3d925cac6487ca3788e78642652074a8ca
Cedric David <cedric.david@btopenworld.com>
2018-09-03 03:46:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,39 +1,31 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-  a = word.upcase.scan(/\b[AEIOU]\w*\b/)
-  letters = word.split("")
-  if a !=[]
-    return true
-  else
-    false
-  end
+   a = word.scan(/\b[aeiou]/i)
+   if a == []
+     false
+   else
+     true
+   end
 end
 
+
 def words_starting_with_un_and_ending_with_ing(text)
-  array = []
-  array = text.scan(/\bun\w*\ing\b/)
-  array
+ res = word.scan(/\bun\w*eng\b/i)
+end
+if res == []
+  false
+else
+  true
 end
 
 def words_five_letters_long(text)
-  array=[]
-  array = text.scan(/\b\w{5}\b/)
-  array
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  sentence = text.match(/\A[A-Z].*\./)
-  if sentence != nil
-    return true
-  else
-    return false
-  end
+
 end
 
 def valid_phone_number?(phone)
-  number = phone.scan(/\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*/)
-  if number != []
-    true
-  else
-    false
-  end
+
 end
