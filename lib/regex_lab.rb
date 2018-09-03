f7dc0645a6feb3bb26ec4cff1595b52f5e95be5e
Cedric David <cedric.david@btopenworld.com>
2018-09-03 04:35:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  text.test(/\D?\d{3}\D?\d{3}\D?\d{4}$/) !=  nil ? true : false
+  phone.match(/\D?\d{3}\D?\d{3}\D?\d{4}$/) !=  nil ? true : false
 end
