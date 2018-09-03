14bf04a134ecbc0981f23e07b7949ec529d2c8e9
Cedric David <cedric.david@btopenworld.com>
2018-09-03 03:56:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,8 +15,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 
 end
 
-  def words_five_letters_long(text)
-  text.scan(\b\w{5}\b)
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
